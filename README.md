92a5ef7234c53cf9e9b1b1e0bd68b290ea93f3e7
extcont3 <50217774+srinidhi-anand@users.noreply.github.com>
2022-03-20 19:58:34 +0530

    Changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# invoicefrontend
+# clientinvoice
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import '../../invoicefrontend/node_modules/bootstrap/dist/css/bootstrap.css'
-import '../../invoicefrontend/node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+import '../../clientinvoice/node_modules/bootstrap/dist/css/bootstrap.css'
+import '../../clientinvoice/node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import VuePaginator from 'vuejs-paginator';
 
