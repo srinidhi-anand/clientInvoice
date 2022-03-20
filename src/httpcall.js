0434dcc3177ab977540487144132a09eba7b4ba9
extcont3 <50217774+srinidhi-anand@users.noreply.github.com>
2022-03-20 23:34:19 +0530

    Update httpcall.js

--- src/httpcall.js
+++ src/httpcall.js
@@ -1,6 +1,6 @@
 import axios from "axios";  
 export default axios.create({  
-  baseURL: "http://sri143.pythonanywhere.com/api",  
+  baseURL: "https://sri143.pythonanywhere.com/api",  
   headers: {  
     "Content-type": "application/json" ,
   }  
